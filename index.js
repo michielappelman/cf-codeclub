95ed91fe18fd451d24786c9eb6782778da3ccc75
Michiel Appelman <michiel@cloudflare.com>
2022-11-08 17:12:30 +0100

    Add switch statement to check the HTTP method diff --git a/index.js b/index.js index 384d539..8d16eee 100644 --- a/index.js +++ b/index.js @@ -1,11 +1,31 @@  export default { -	fetch(request, env) { -    console.log(request); +  async fetch(request, env) {      console.log(env); -		return new Response('Hello worker!', { -			headers: { -				'content-type': 'text/plain', -			}, -		}); -	}, +    switch (request.method) { +      case "GET": +        return new Response("Hello to GET a worker!", { +          headers: { +            "content-type": "text/plain", +          }, +        }); +      case "POST": { +        let response = { message: "Hello to POST worker", error: false }; +        return new Response(JSON.stringify(response), { +          headers: { +            "content-type": "application/json", +          }, +        }); +      } +      // I need curly braces here because the `response` variable is already +      // declared above and is not scoped to the `case` block by default. +      default: { +        let response = { message: "Unsupported HTTP method", error: true }; +        return new Response(JSON.stringify(response), { +          headers: { +            "content-type": "text/plain", +          }, +        }); +      } +    } +  },  };

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 export default {
-	fetch(request, env) {
-    console.log(request);
+  async fetch(request, env) {
     console.log(env);
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
+    switch (request.method) {
+      case "GET":
+        return new Response("Hello to GET a worker!", {
+          headers: {
+            "content-type": "text/plain",
+          },
+        });
+      case "POST": {
+        let response = { message: "Hello to POST worker", error: false };
+        return new Response(JSON.stringify(response), {
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+      }
+      // I need curly braces here because the `response` variable is already
+      // declared above and is not scoped to the `case` block by default.
+      default: {
+        let response = { message: "Unsupported HTTP method", error: true };
+        return new Response(JSON.stringify(response), {
+          headers: {
+            "content-type": "text/plain",
+          },
+        });
+      }
+    }
+  },
 };
