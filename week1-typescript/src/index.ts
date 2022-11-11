69ca866f824b536404b4eb92a0c12439eee24d90
Michiel Appelman <michiel@cloudflare.com>
2022-11-11 10:36:59 +0100

    Add full TypeScript rewrite of week1 diff --git a/week1-typescript/package.json b/week1-typescript/package.json new file mode 100644 index 0000000..2e639fc --- /dev/null +++ b/week1-typescript/package.json @@ -0,0 +1,16 @@ +{ +	"private": true, +	"version": "0.0.0", +	"scripts": { +		"deploy": "wrangler publish index.js", +		"dev": "wrangler dev index.js --local" +	}, +	"devDependencies": { +		"wrangler": "^2.0.0" +	}, +	"dependencies": { +		"@cloudflare/workers-types": "^3.18.0", +		"@cloudflare/wrangler": "^1.20.0", +		"typescript": "^4.8.4" +	} +} diff --git a/week1-typescript/src/index.ts b/week1-typescript/src/index.ts index 0ddd687..d28197b 100644 --- a/week1-typescript/src/index.ts +++ b/week1-typescript/src/index.ts @@ -1,15 +1,57 @@ -/** - * Welcome to Cloudflare Workers! This is your first worker. - * - * - Run `wrangler dev src/index.ts` in your terminal to start a development server - * - Open a browser tab at http://localhost:8787/ to see your worker in action - * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker - * - * Learn more at https://developers.cloudflare.com/workers/ - */ +interface Environment { +  DEBUG: boolean; +} + +interface JSONResponse { +  message: string; +  error: boolean; +} + +type ResponseMessage = string | JSONResponse;
     export default {
    -  async fetch(request: Request): Promise<Response> {
    -    return new Response("Hello World!");
    +  async fetch(request: Request, env: Environment): Promise<Response> {
    +    // Default reply will be a JSON message and if we didn't change it,
    +    // something probalby went wrong.
    +    let response: ResponseMessage = {
    +      message: "Something went wrong.",
    +      error: true,
    +    };
    +
    +    if (env.DEBUG) {
    +      console.log(`HTTP Method: ${request.method}`);
    +    }
    +    switch (request.method) {
    +      case "GET":
    +        response = "Hello to you in plain text!";
    +        break;
    +      case "POST":
    +        const name = new URL(request.url).searchParams.get("name");
    +        if (env.DEBUG) {
    +          console.log(`name: ${name}`);
    +        }
    +
    +        if (!name) {
    +          response = { message: "Missing parameter `name`", error: true };
    +          break;
    +        }
    +
    +        response = { message: `Hello to you, ${name}!`, error: false };
    +        break;
    +      default:
    +        response = { message: "Unsupported HTTP method", error: true };
    +    }
    +
    +    // If the response is an object, it's safe to assume it should be returned
    +    // as JSON. Otherwise just return it as plain text.
    +    if (typeof response === "object") {
    +      return new Response(JSON.stringify(response), {
    +        headers: { "content-type": "application/json" },
    +      });
    +    } else {
    +      return new Response(response, {
    +        headers: { "content-type": "text/plain" },
    +      });
    +    }
       },
     };
    diff --git a/week1-typescript/wrangler.toml b/week1-typescript/wrangler.toml
    index 37f5189..ff7134d 100644
    --- a/week1-typescript/wrangler.toml
    +++ b/week1-typescript/wrangler.toml
    @@ -1,3 +1,6 @@
     name = "week1-typescript"
     main = "src/index.ts"
     compatibility_date = "2022-11-11"
    +
    +[vars]
    +DEBUG = 1

--- week1-typescript/src/index.ts
+++ week1-typescript/src/index.ts
@@ -1,15 +1,57 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+interface Environment {
+  DEBUG: boolean;
+}
+
+interface JSONResponse {
+  message: string;
+  error: boolean;
+}
+
+type ResponseMessage = string | JSONResponse;
 
 export default {
-  async fetch(request: Request): Promise<Response> {
-    return new Response("Hello World!");
+  async fetch(request: Request, env: Environment): Promise<Response> {
+    // Default reply will be a JSON message and if we didn't change it,
+    // something probalby went wrong.
+    let response: ResponseMessage = {
+      message: "Something went wrong.",
+      error: true,
+    };
+
+    if (env.DEBUG) {
+      console.log(`HTTP Method: ${request.method}`);
+    }
+    switch (request.method) {
+      case "GET":
+        response = "Hello to you in plain text!";
+        break;
+      case "POST":
+        const name = new URL(request.url).searchParams.get("name");
+        if (env.DEBUG) {
+          console.log(`name: ${name}`);
+        }
+
+        if (!name) {
+          response = { message: "Missing parameter `name`", error: true };
+          break;
+        }
+
+        response = { message: `Hello to you, ${name}!`, error: false };
+        break;
+      default:
+        response = { message: "Unsupported HTTP method", error: true };
+    }
+
+    // If the response is an object, it's safe to assume it should be returned
+    // as JSON. Otherwise just return it as plain text.
+    if (typeof response === "object") {
+      return new Response(JSON.stringify(response), {
+        headers: { "content-type": "application/json" },
+      });
+    } else {
+      return new Response(response, {
+        headers: { "content-type": "text/plain" },
+      });
+    }
   },
 };
