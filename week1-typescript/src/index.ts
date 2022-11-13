485fdef2b4ac98f20645a8a823da99262efa4e82
Michiel Appelman <michiel@cloudflare.com>
2022-11-13 22:21:18 +0100

    Rewrite to leverage htmx.org

--- week1-typescript/src/index.ts
+++ week1-typescript/src/index.ts
@@ -2,56 +2,47 @@ interface Environment {
   DEBUG: boolean;
 }
 
-interface JSONResponse {
-  message: string;
-  error: boolean;
-}
-
-type ResponseMessage = string | JSONResponse;
-
 export default {
   async fetch(request: Request, env: Environment): Promise<Response> {
-    // Default reply will be a JSON message and if we didn't change it,
-    // something probalby went wrong.
-    let response: ResponseMessage = {
-      message: "Something went wrong.",
-      error: true,
-    };
+    let response: string;
 
     if (env.DEBUG) {
       console.log(`HTTP Method: ${request.method}`);
     }
     switch (request.method) {
       case "GET":
-        response = "Hello to you in plain text!";
+        response = `<!DOCTYPE html><html><head>
+<meta charset="utf-8">
+<title>Michiel's Hi-sayer</title>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/htmx/1.8.4/htmx.min.js"></script>
+</head>
+<body><center><form id="nameform" hx-post="/" hx-target="#nameform" hx-swap="outerHTML">
+  <label for="name">Your name:</label><br>
+  <input type="text" id="name" name="name">
+  <button type="submit">Say Hi!</button>
+</form></center></body>
+</html>`;
         break;
       case "POST":
-        const name = new URL(request.url).searchParams.get("name");
+        const data = await request.formData();
+        const name = data.get("name");
         if (env.DEBUG) {
-          console.log(`name: ${name}`);
+          console.log(name);
         }
 
         if (!name) {
-          response = { message: "Missing parameter `name`", error: true };
+          response = "<h2>Please provide your name...</h2>";
           break;
         }
 
-        response = { message: `Hello to you, ${name}!`, error: false };
+        response = `<h1>Hi, ${name}!</h1>`;
         break;
       default:
-        response = { message: "Unsupported HTTP method", error: true };
+        response = "<html><body><h2>Unsupported method</h2></body></html>";
     }
 
-    // If the response is an object, it's safe to assume it should be returned
-    // as JSON. Otherwise just return it as plain text.
-    if (typeof response === "object") {
-      return new Response(JSON.stringify(response), {
-        headers: { "content-type": "application/json" },
-      });
-    } else {
-      return new Response(response, {
-        headers: { "content-type": "text/plain" },
-      });
-    }
+    return new Response(response, {
+      headers: { "content-type": "text/html" },
+    });
   },
 };
