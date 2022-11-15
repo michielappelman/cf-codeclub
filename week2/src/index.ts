a5274ae01c9e9410b56a776f45b7fe7cb175a2a5
Michiel Appelman <michiel@cloudflare.com>
2022-11-15 21:45:21 +0100

    Add week2 showing you a random Rick

---
+++ week2/src/index.ts
@@ -0,0 +1,67 @@
+import { html, HTMLContent, HTMLResponse } from "@worker-tools/html";
+
+export interface Env {
+  DEBUG: boolean;
+}
+
+const ricks = [
+  1, // regular
+  265, // pickle
+  220, // mega fruit farmer
+  482, // secret service
+];
+
+type Rick = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  image: string;
+};
+
+const baseLayout = (content: HTMLContent) => html`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Random Rick</title>
+      <link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css" />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/htmx/1.8.4/htmx.min.js"></script>
+    </head>
+    <body>
+      <header>
+        <h1>Your random Rick is:</h1>
+      </header>
+      <main>
+        ${content}
+        <footer>
+          <a href="https://github.com/michielappelman/cf-codeclub/">Source</a>
+        </footer>
+      </main>
+    </body>
+  </html>`;
+
+async function getRick(id: number): Promise<Rick> {
+  const rickRequest = await fetch(
+    `https://rickandmortyapi.com/api/character/${id}`
+  );
+  return rickRequest.json();
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const randomInt = Math.floor(Math.random() * 4);
+    const randomRick = await getRick(ricks[randomInt]);
+    return new HTMLResponse(
+      baseLayout(
+        html`<section>
+          <aside>
+            <figure><img src="${randomRick.image}" /></figure>
+            <br />
+            <h3>${randomRick.name}</h3>
+          </aside>
+        </section>`
+      )
+    );
+  },
+};
